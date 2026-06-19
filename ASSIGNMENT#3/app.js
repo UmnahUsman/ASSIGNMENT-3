0fce973004df9dd08c7007288ec63e4337ac0175
UmnahUsman <umnahusman1@gmail.com>
2026-06-19 21:22:38 +0500

    Add files via upload

---
+++ ASSIGNMENT#3/app.js
@@ -0,0 +1,25 @@
+// QUESTION#1
+
+var age = "16";
+alert("I  am" + " " + age + " " + "years old");
+
+// QUESTION#2
+
+ var time = "14";
+ alert("You have visited this site" + " " + time + " " + "times");
+
+//  QUESTION#3
+
+var birthYear = "2010";
+document.write("My birth year is " + " " + birthYear + "<br>");
+
+
+var type = "number";
+document.write("Data type of my declared variables is" + " " + type + "<br>");
+
+// QUESTION#4
+
+var visitorName = "Jhon Doe";
+ product = "T-Shirt(s)";
+ quantity = "5";
+ document.write(visitorName + " " + "ordered" + " " + quantity + " " + product + "on XYZ Clothing store");
