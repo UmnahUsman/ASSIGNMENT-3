fe6f022c1ff9bebc10ecda2fd4fcde6135d183d2
UmnahUsman <umnahusman1@gmail.com>
2026-06-20 17:21:05 +0500

    Add files via upload

---
+++ CHAPTER#4/app.js
@@ -0,0 +1,13 @@
+// QUESTION#1
+
+// var visitorName = "Jhon Doe";
+//  product = "T-Shirt(s)";
+//  quantity = "5";
+//  document.write(visitorName + " " + "ordered" + " " + quantity + " " + product + "on XYZ Clothing store");
+
+//  QUESTION#2
+
+var legalVariables = "camelCase";"snake_case";"dollarsign$";"can2HaveNumbers";"_startWithUnderscore";
+IllegalVariables = "2CannotStartWithANumber";"cannot-usesdashes";"cannot have spaces";"letcannot uses JS keywords";"!cannot uses special characters";
+
+
